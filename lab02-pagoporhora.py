b309bed0bd62b217b154ef19244c6d4afd580326
robertominuche <112092136+robertominuche@users.noreply.github.com>
2022-09-28 21:38:45 -0600

    Delete lab02-pagoporhora.py

--- lab02-pagoporhora.py
+++
@@ -1,6 +0,0 @@
-#tendencias e innovaciones de tecnologias agricolas
-Horas = input("¿Cuantás horas trabaja? ")
-Pago = input ("Pago por hora: ")
-
-Total = float(Horas) * float(Pago)
-print("su total es", Total)
